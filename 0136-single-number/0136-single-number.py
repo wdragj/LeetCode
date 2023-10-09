8aaa21a9f7b3e46f201d092e03a8cd59da679af7
Freddy Seo <wdragj@gmail.com>
2023-10-08 23:25:46 -0500

    Time: 117 ms (87.59%), Space: 18.9 MB (63.6%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,13 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        numDict = {}
-        for n in nums:
-            if n in numDict:
-                numDict[n] += 1
-            else:
-                numDict[n] = 1
-        
-        for itms in numDict.items():
-            k, v = itms
-            if v == 1:
-                return k
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
