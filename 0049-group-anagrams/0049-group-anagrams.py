936bbd13fa433d63247fc68adf393dfa871b93d3
Freddy Seo <wdragj@gmail.com>
2023-10-09 23:06:00 -0500

    Time: 99 ms (60.74%), Space: 19.5 MB (90.53%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for word in strs:
+            sortedWord = "".join(sorted(word))
+            if sortedWord in anagrams:
+                anagrams[sortedWord].append(word)
+            else:
+                anagrams[sortedWord] = [word]
+        return list(anagrams.values())
