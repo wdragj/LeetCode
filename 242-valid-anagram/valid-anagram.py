f4f38b4bf93217a216116ece9ad98877ec46dcdf
Freddy Seo <wdragj@gmail.com>
2024-03-15 01:43:06 -0500

    Time: 52 ms (43.21%) | Memory: 17 MB (68.69%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        sHash, tHash = {}, {}
+
+        for c_s, c_t in zip(s, t):
+            sHash[c_s] = sHash.get(c_s, 0) + 1
+            tHash[c_t] = tHash.get(c_t, 0) + 1
+        
+        for c in s:
+            if sHash[c] != tHash.get(c, 0):
+                return False
+        
+        return True
