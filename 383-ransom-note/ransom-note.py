458b96d168c30e0cf7896120da81c253ca9985de
Freddy Seo <wdragj@gmail.com>
2024-03-15 02:20:19 -0500

    Time: 70 ms (18.38%) | Memory: 16.7 MB (85.37%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashDict = {}
+
+        for c in magazine:
+            if c not in hashDict:
+                hashDict[c] = 1
+            else:
+                hashDict[c] += 1
+
+        for c in ransomNote:
+            if c not in hashDict:
+                return False
+            else:
+                if hashDict.get(c) == 0:
+                    return False
+                else:
+                    hashDict[c] -= 1
+        
+        return True
