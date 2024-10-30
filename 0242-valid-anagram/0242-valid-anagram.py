c20ac846e1d89b224021ab9d614a2538f1fc52ec
Freddy Seo <wdragj@gmail.com>
2024-10-29 20:41:06 -0500

    Time: 11 ms (76.05%), Space: 16.6 MB (95.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,13 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hash_s, hash_t =  {}, {}
-        longer = s if len(s) > len(t) else t
+        freq_s = defaultdict(int)
+        freq_t = defaultdict(int)
+
         for c in s:
-            if c not in hash_s:
-                hash_s[c] = 1
-            else:
-                hash_s[c] += 1
-        
+            freq_s[c] += 1
         for c in t:
-            if c not in hash_t:
-                hash_t[c] = 1
-            else:
-                hash_t[c] += 1
-        
-        for c in longer:
-            if hash_s.get(c, None) != hash_t.get(c, None):
-                return False
-        
-        return True
+            freq_t[c] += 1
+
+        return True if freq_s.items() == freq_t.items() else False
