290b50df68ec846b5d6126c70ced2da0c63df2f9
Freddy Seo <wdragj@gmail.com>
2024-11-04 20:55:46 -0600

    Time: 0 ms (100%), Space: 17.4 MB (97.02%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Two pointers l and r
         l, r = 0, len(numbers) - 1
 
         while l <= r:
             sumNum = numbers[l] + numbers[r]
 
-            # Equals target
+            # If target, return
             if sumNum == target:
                 return [l + 1, r + 1]
-            
-            # Less than target
+
+            # If less than target increment l
             if sumNum < target:
                 l += 1
-            
-            # Greater than target
+
+            # If greater than target decrement r
             if sumNum > target:
                 r -= 1
-            
