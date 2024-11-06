6207be4adefc65baa54538494075e6e99901f49e
Freddy Seo <wdragj@gmail.com>
2024-11-06 15:13:15 -0600

    Time: 235 ms (22.4%), Space: 38.3 MB (56.51%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,11 +1,9 @@
-from collections import deque
-
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [[p, s] for p, s in zip(position, speed)]
+        pairs = [[p, s] for p, s in zip(position, speed)]
 
         stack = []
-        for p, s in sorted(pair)[::-1]:
+        for p, s in sorted(pairs)[::-1]:
             stack.append((target - p) / s)
 
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
