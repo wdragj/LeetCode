a91888627e1f82a08391e7a5b2b23a7360fedd2d
Freddy Seo <wdragj@gmail.com>
2024-11-06 23:09:31 -0600

    Time: 2 ms (5.36%), Space: 17.1 MB (25.05%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,11 +1,19 @@
-from collections import deque
-
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
-        d = deque(nums)
+        ans = nums[0]
         
-        while d[l] > d[r]:
-            d.rotate(1)
+        while l <= r:
+            if nums[l] < nums[r]:
+                ans = min(ans, nums[l])
+                break
+            
+            mid = (l + r) // 2
+            ans = min(ans, nums[mid])
+
+            if nums[mid] >= nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
         
-        return d[0]
+        return ans
