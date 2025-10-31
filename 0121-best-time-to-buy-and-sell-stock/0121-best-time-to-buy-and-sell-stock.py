ae3b823eafd81786f7130848227fbf2a444e646f
Freddy Seo <wdragj@gmail.com>
2025-10-31 01:57:44 -0500

    Time: 109 ms (40.97%), Space: 27.5 MB (8.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Start from the last day and work backwards
-        dp = [0] * len(prices)
+        n = len(prices)
+        dp = [0] * n
+        dp[-1] = 0
         max_sell = prices[-1]
 
-        for i in range(len(prices) - 2, -1, -1):
-            if prices[i] > max_sell:
-                max_sell = prices[i]
-                dp[i] = 0
-            else:
+        for i in range(n - 2, -1, -1):
+            if prices[i] <= max_sell:
                 dp[i] = max_sell - prices[i]
-
+            
+            # Update max sell
+            max_sell = max(prices[i], max_sell)
+        
         return max(dp)
