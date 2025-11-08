4e20625e44f1a16aa3b53c9d016a79e312bde92b
Freddy Seo <wdragj@gmail.com>
2025-11-08 17:20:29 -0600

    Time: 0 ms (100%), Space: 18 MB (18.16%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -2,35 +2,24 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         if not matrix or not matrix[0]:
             return []
-        m = len(matrix)
-        n = len(matrix[0])
+        
         spiral_order = []
-        right, down, left, up = n, m, 0, 0
+        while matrix:
+            # 1) Add first row
+            spiral_order += matrix.pop(0)
 
-        while True:
-            # Scan right
-            for r in range(left, right):
-                spiral_order.append(matrix[up][r])
-            up += 1
-            if len(spiral_order) == (n * m):
-                break
-            # Scan down
-            for d in range(up, down):
-                spiral_order.append(matrix[d][right - 1])
-            right -= 1
-            if len(spiral_order) == (n * m):
-                break
-            # Scan left
-            for l in range(right - 1, left - 1, -1):
-                spiral_order.append(matrix[down - 1][l])
-            down -= 1
-            if len(spiral_order) == (n * m):
-                break
-            # Scan up
-            for u in range(down - 1, up - 1, -1):
-                spiral_order.append(matrix[u][left])
-            left += 1
-            if len(spiral_order) == (n * m):
-                break
+            # 2) Add all last elements in order
+            if matrix and matrix[0]:
+                for row in matrix:
+                    spiral_order.append(row.pop())
 
+            # 3) Add reverse of last row
+            if matrix:
+                spiral_order += matrix.pop()[::-1]
+
+            # 4) Add first element of all row in reverse
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    spiral_order.append(row.pop(0))
+        
         return spiral_order
