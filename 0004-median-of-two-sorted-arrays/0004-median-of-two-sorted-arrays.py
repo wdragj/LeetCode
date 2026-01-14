e4230555609cee919783a7bb2fe065e3a96d70a9
Freddy Seo <wdragj@gmail.com>
2026-01-13 20:15:55 -0600

    Time: 0 ms (100%), Space: 19.6 MB (6.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,15 +1,29 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # Brute Force
-        merged_list = nums1 + nums2
-        merged_list.sort()
+        if len(nums2) < len(nums1):
+            nums1, nums2 = nums2, nums1
+        n, m = len(nums1), len(nums2)
+        total = n + m
+        half = total // 2
 
-        # Mid idx
-        mid = len(merged_list) // 2
+        left, right = 0, n
+        while left <= right:
+            i = (left + right) // 2
+            j = half - i
 
-        # If even
-        if len(merged_list) % 2 == 0:
-            return (merged_list[mid - 1] + merged_list[mid])/2
-        else:
-            # Odd
-            return merged_list[mid]
+            l1 = nums1[i - 1] if i > 0 else float('-inf')
+            l2 = nums2[j - 1] if j > 0 else float('-inf')
+
+            r1 = nums1[i] if i < n else float('inf')
+            r2 = nums2[j] if j < m else float('inf')
+
+            if l1 > r2:
+                right = i - 1
+            elif l2 > r1:
+                left = i + 1
+            else:
+                if total % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                else:
+                    return min(r1, r2)
+        return 0.0
