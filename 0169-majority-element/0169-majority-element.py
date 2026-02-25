458ac3534b3e906e3b65db1ddf40b2a329dc309a
Freddy Seo <wdragj@gmail.com>
2026-02-25 16:38:06 -0600

    Time: 5 ms (65.27%), Space: 21.2 MB (41.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        l, r = 0, len(nums)
-        mid = (l + r) // 2
-        nums.sort()
-        return nums[mid]
+        res = count = 0
+        for num in nums:
+            if count == 0:
+                res = num
+            count += (1 if res == num else -1)
+        return res
