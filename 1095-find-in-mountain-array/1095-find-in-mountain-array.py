30e3cb0de09ea66847f7ad8a0e7daee62504cbcc
Freddy Seo <wdragj@gmail.com>
2026-02-26 20:11:41 -0600

    Time: 44 ms (64.47%), Space: 20.3 MB (20.45%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -1,47 +1,39 @@
-# """
-# This is MountainArray's API interface.
-# You should not implement it, or speculate about its implementation
-# """
-#class MountainArray:
-#    def get(self, index: int) -> int:
-#    def length(self) -> int:
-
 class Solution:
     def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
         n = mountainArr.length()
-        
-        # Find peak
-        left, right = 0, n - 1
-        while left < right:
-            mid = (left + right) // 2
-            if mountainArr.get(mid) < mountainArr.get(mid + 1):
-                left = mid + 1
+        l, r = 0, n
+        peak = -1
+        while l < r:
+            mid = (l + r) // 2
+            mid_val = mountainArr.get(mid)
+            r_val = mountainArr.get(mid + 1) if mid + 1 < n else float("-inf")
+            if mid_val > r_val:
+                r = mid
+                peak = mid
             else:
-                right = mid
-        peak = left
-        
-        # Search ascending part [0, peak]
-        left, right = 0, peak
-        while left <= right:
-            mid = (left + right) // 2
-            val = mountainArr.get(mid)
-            if val == target:
+                l = mid + 1
+        if target == mountainArr.get(peak):
+            return peak
+
+        l, r = 0, peak - 1
+        while l <= r:
+            mid = (l + r) // 2
+            mid_val = mountainArr.get(mid)
+            if mid_val == target:
                 return mid
-            elif val < target:
-                left = mid + 1
+            elif mid_val < target:
+                l = mid + 1
             else:
-                right = mid - 1
+                r = mid - 1
         
-        # Search descending part [peak+1, n-1]
-        left, right = peak + 1, n - 1
-        while left <= right:
-            mid = (left + right) // 2
-            val = mountainArr.get(mid)
-            if val == target:
+        l, r = n - 1, peak + 1
+        while l >= r:
+            mid = (l + r) // 2
+            mid_val = mountainArr.get(mid)
+            if mid_val == target:
                 return mid
-            elif val > target:  # Reversed for descending
-                left = mid + 1
+            elif mid_val < target:
+                l = mid - 1
             else:
-                right = mid - 1
-        
+                r = mid + 1
         return -1
